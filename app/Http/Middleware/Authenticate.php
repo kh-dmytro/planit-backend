3d694dda3794f6d5d764c49bb407c1e4f7c8b74a
kh-dmytro <diptrade869@gmail.com>
2024-11-01 17:55:48 +0200

    fixed authentification check

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,10 +4,13 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse; // Добавлен импорт для JsonResponse
+use Closure; // Используем глобальный \Closure, а не App\Http\Middleware\Closure
+use Illuminate\Support\Facades\Log;
 
 class Authenticate extends Middleware
 {
-    /**
+     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
     /*
@@ -16,8 +19,26 @@ class Authenticate extends Middleware
         return $request->expectsJson() ? null : route('login');
     }
         */
-    protected function unauthenticated($request, array $guards)
+
+        /*
+        protected function unauthenticated($request, array $guards)
+        {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+        */
+
+    public function handle($request, Closure $next, ...$guards)
     {
-        return response()->json(['error' => 'Unauthenticated.'], 401);
+        \Log::info('Sanctum Middleware: Checking user auth');
+        try {
+            $this->authenticate($request, $guards);
+        } catch (\Illuminate\Auth\AuthenticationException $e) {
+            \Log::info('Sanctum Middleware: Unauthorized access');
+            return new JsonResponse([
+                'error' => 'Unauthorized',
+                'message' => 'Invalid or missing token'
+            ], 401);
+        }
+        return $next($request);
     }
 }
